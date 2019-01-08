d7e596b40f0450eac43b8d42dbd428a8b0c81034
hughtain <hxt1wed2@gmail.com>
2019-01-08 21:12:54 +0800

    imu_hasas_bug

---
+++ stm32f103c8t6_ws281x_spi+dma_branch_imu/User/main.c
@@ -0,0 +1,100 @@
+#include "stm32f10x.h"
+#include "stm32f10x_it.h"
+#include "bsp_usart1.h"
+#include "delay.h"
+#include "bsp_i2c.h"
+#include "../BOARD/ws2812/ws2812.h"
+#include "../BOARD/mpu6050/mpu6050.h"
+#include "../BOARD/mpu6050/IMU.h"
+
+float  Angle_ax=0.0,Angle_ay=0.0,Angle_az=0.0;  //由加速度计算的加速度(弧度制)
+float  Angle_gy=0.0,Angle_gx=0.0,Angle_gz=0.0;  //由角速度计算的角速率(角度制)
+
+short Acel[3];
+short Gyro[3];
+
+int main(void)
+{
+//mpu6050 data buff
+
+//	short Temp;
+
+	USART1_Config();
+	NVIC_Configuration();
+	delay_init();
+	ws281x_init();
+	i2c_GPIO_Config();
+	MPU6050_Init();
+
+	ws281x_closeAll();
+
+  while(1)
+  {
+
+	MPU6050ReadAcc(Acel);
+	MPU6050ReadGyro(Gyro);
+	  
+	Angle_ax = Acel[0] / 8192.0;
+	Angle_ay = Acel[1] / 8192.0;
+	Angle_az = Acel[2] / 8192.0;
+	Angle_gx = Gyro[0] / 65.5*0.0174533;
+	Angle_gy = Gyro[1] / 65.5*0.0174533;
+	Angle_gz = Gyro[2] / 65.5*0.0174533;
+
+	Ang_Dat Ang= IMUupdate(Angle_gx,Angle_gy,Angle_gz,Angle_ax,Angle_ay,Angle_az);
+	  
+	if(Ang.Angley_dat > (0.0) && Ang.Angley_dat <= (15.0))
+	{
+		ws281x_colorWipe(ws281x_color(0, 10, 10), 15);
+		printf("\r\n angley dat :   %f\r\n", Ang.Angley_dat);
+		printf("\r\n angle dat :   %f\r\n", Ang.Angle_dat);
+		// printf("angley dat :   %f \r\n", Ang.Angley_dat);
+		printf("\r\n");
+		printf("\r\n");
+	}	 
+	  
+	else if(Ang.Angley_dat > (-15.0) && Ang.Angley_dat <= (0.0))
+	{
+		ws281x_colorWipe(ws281x_color(10, 10, 10), 15);
+		printf("\r\n angley dat :   %f\r\n", Ang.Angley_dat);
+		printf("\r\n angle dat :   %f\r\n", Ang.Angle_dat);
+		// printf("angley dat :   %f \r\n", Ang.Angley_dat);
+		printf("\r\n");
+		printf("\r\n");
+	}
+	
+	else if(Ang.Angley_dat > (-30.0) && Ang.Angley_dat <= (0.0))
+	{
+		ws281x_colorWipe(ws281x_color(10, 0, 0), 15);
+		printf("\r\n angley dat :   %f\r\n", Ang.Angley_dat);
+		// printf("angley dat :   %f \r\n", Ang.Angley_dat);
+		printf("\r\n");
+		printf("\r\n");
+	}
+	
+}
+}
+
+
+//ws281x_rainbow(200);
+
+//	while(1)
+//	{
+//		//  ws281x_colorWipe(ws281x_color(0, 255, 0), 50); // Green
+//		ws281x_colorWipe(ws281x_color(0, 0, 10), 50); // Blue
+//		ws281x_colorWipe_rollback(ws281x_color(10, 0, 0), 50);
+////colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
+//  // Send a theater pixel chase in...
+
+
+//		ws281x_rainbow(20);
+////		ws281x_rainbowCycle(20);
+////		ws281x_theaterChaseRainbow(200);
+//	}
+
+
+
+//	ws281x_theaterChase(ws281x_color(20, 25, 30), 50); // White
+//	ws281x_theaterChase(ws281x_color(20, 0, 30), 50); 
+//	ws281x_theaterChase(ws281x_color(10, 55, 35), 50); // Blue
+
---
+++ stm32f103c8t6_ws281x_spi+dma_branch_imu/User/run.h
@@ -0,0 +1,6 @@
+#ifndef	__RUN_H
+#define __RUN_H
+
+void run_my_setup(void);
+
+#endif 
